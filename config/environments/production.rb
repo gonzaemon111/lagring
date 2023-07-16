51748c6326d426aac6a694a12dc8adb5a37c4f25
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-07-16 22:13:49 +0900

    初期設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.hosts << 'rails-production-b73c.up.railway.app'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -22,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
