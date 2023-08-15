da925f5d1e02e4a8663e7feafdcf192a3fe4c74d
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-08-16 07:50:43 +0900

    一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, only: %i[show create]
-    resources :domains, only: %i[index show create destroy update]
-    resources :tasks, only: %i[index show create destroy update]
-    resources :daily_necessities, only: %i[index show create destroy update]
+    resources :users, only: %i[show create], defaults: { format: :json }
+    resources :domains, only: %i[index show create destroy update], defaults: { format: :json }
+    resources :tasks, only: %i[index show create destroy update], defaults: { format: :json }
+    resources :daily_necessities, only: %i[index show create destroy update], defaults: { format: :json }
     resources :checklists, only: %i[index show create destroy update], defaults: { format: :json }
     resources :subscriptions, only: %i[index show create destroy update], defaults: { format: :json }
-    resources :shoppings, only: %i[index show create destroy update], defaults: { format: :json }
   end
 
   get 'health', to: 'health#check'
