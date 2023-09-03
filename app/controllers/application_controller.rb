a638a45390826fc719684cb3cd82fff82ade479d
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-09-03 18:37:41 +0900

    🔴 AccessLogを落とすようにする close #58

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  after_action :record_access_log
+
   rescue_from ::InvalidParametersError, with: :rescue_error
   rescue_from ::NotFoundError, with: :rescue_error
 
@@ -11,5 +13,22 @@ class ApplicationController < ActionController::Base
 
   def rescue_error(error)
     render(status: error.code || 400, json: { message: error.message, code: error.code, messages: error.details })
+  end
+
+  def record_access_log
+    return unless ::Rails.env.production?
+
+    ::Rails.logger.info({
+      type: 'access',
+      env: ::Rails.env.to_s,
+      path: request.fullpath,
+      params: request.params,
+      ip: request.remote_ip,
+      request_uuid: request.uuid,
+      referer: request.referer,
+      format: request.format,
+      response_format: response.content_type,
+      status: response.status
+    }.to_json)
   end
 end
